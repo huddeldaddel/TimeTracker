0a9025a4620e5e551d098914fd11ca8c88d89449
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2024-03-08 20:11:56 +0100

    Fixed some additional warnings

---
+++ TimeTracker/Exceptions/DbInitializationFailedException.cs
@@ -0,0 +1,10 @@
+﻿namespace TimeTracker.Exceptions
+{
+    public class DbInitializationFailedException : IOException
+    {
+        public DbInitializationFailedException() : base("Failed to initialize DB connection")
+        {
+
+        }
+    }
+}
